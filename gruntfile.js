ac927446157a87e61d732ac3a7548f96dbfe5ddd
Gieryn <gierynt@gmail.com>
2017-10-01 20:38:02 +0200

    Modyfied grunt watch, added browser-sync

--- gruntfile.js
+++ gruntfile.js
@@ -1,50 +1,74 @@
 module.exports = function(grunt) {
 
-  // Project configuration
-  grunt.initConfig({
-  	sass: {
-  		options: {
-  			sourceMap: true
-  		},
-  		dist: {
-  			files: {
-  				'css/main.css': 'sass/main.sass'
-  			}
-  		}
-  	},
-
-  	imagemin: {
-      dynamic: {
-        files: [{
-          expand: true,
-          cwd: 'images/',
-          src: ['**/*.{png,jpg,gif,jpeg}'],
-          dest: 'images-build/'
-        }]
-      }
-    },
-
-
-    watch: {
-    scripts: {
-        files: ['sass/*.sass', 'images/*'],
-        tasks: ['sass', 'imagemin'],
-        options: {
-            spawn: false,
+    // Project configuration
+    grunt.initConfig({
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'css/main.css': 'sass/main.sass'
+                }
+            }
         },
-    }
-}
+
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'images/',
+                    src: ['**/*.{png,jpg,gif,jpeg}'],
+                    dest: 'images-build/'
+                }]
+            }
+        },
+
+        watch: {
+            styles: {
+                files: ['sass/*.sass'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false,
+                },
+            },
+
+            images: {
+                files: ['images/*'],
+                tasks: ['imagemin'],
+                options: {
+                    spawn: false,
+                },
+            }
+        },
+
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src: [
+                        'css/*.css',
+                        '*.html'
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: './'
+                }
+            }
+        }
+
+    });
+
+    
+    // Load the plugins tasks
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-browser-sync');
 
 
-  });
-  // Load the plugins tasks
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-  
 
+    // Default task(s).
 
-  // Default task(s).
-
-  grunt.registerTask('default',['sass', 'imagemin', 'watch']);
+    grunt.registerTask('default', ['sass', 'imagemin', 'browserSync', 'watch']);
 };
